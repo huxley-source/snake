41cb45e704c5432071ed20fbc9b622a729fb7851
Huxley Source <r.krzywaznia@gmail.com>
2017-04-21 10:07:56 +0200

    small fix

--- src/cljs/snake/utils.cljs
+++ src/cljs/snake/utils.cljs
@@ -1,17 +1,15 @@
 (ns snake.utils)
 
-(def key-code->move                                         ;przekształcenie kodu klawisza na wektor []
+(def key-code->move                                         ;przekształcenie kodu klawisza na wektor [#{-1 0 1} #{-1 0 1}]
   {37 [0 -1]                                                ;strzałka v
    38 [-1 0]                                                ;strzałka <
    39 [0 1]                                                 ;strzałka ^
    40 [1 0]})                                               ;strzałka >
 
-(defn random-coords
-  [[rows cols]]
+(defn random-coords [[rows cols]]
   [(rand-int rows) (rand-int cols)])
 
-(defn get-time
-  []
+(defn get-time []
   (let [millis (.getTime (js/Date.))]
     (/ millis 1000)))
 
